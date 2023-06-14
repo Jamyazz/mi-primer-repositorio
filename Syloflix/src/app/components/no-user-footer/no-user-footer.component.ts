5a0bf46ab693710ae6866a06d7292c6e076c9e03
gmanselmo <130943341+gmanselmo@users.noreply.github.com>
2023-06-14 16:44:01 -0300

    Delete Syloflix directory

--- Syloflix/src/app/components/no-user-footer/no-user-footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-no-user-footer',
-  templateUrl: './no-user-footer.component.html',
-  styleUrls: ['./no-user-footer.component.scss']
-})
-export class NoUserFooterComponent {
-}
