12341344104d82f631a98f443b75ad8adfa1d940
Germán Anselmo <germanmanselmo@gmail.com>
2023-06-14 17:31:46 -0300

    feat: configuracion y automatización de workspace en Angular. feat: creación de vista login, sus componentes. Maquetado vista index.

---
+++ Syloflix/src/app/modules/auth/components/login-box/login-box.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-login-box',
+  templateUrl: './login-box.component.html',
+  styleUrls: ['./login-box.component.scss']
+})
+export class LoginBoxComponent {
+
+  @ViewChild('elcorreo') mail!: ElementRef;
+  @ViewChild('lapassword') pass!: ElementRef;
+
+  cambiar(): void {
+
+    const elcorreo: any = document.querySelector("#email");
+    elcorreo.style.color= "red";
+
+  }
+
+}
---
+++ Syloflix/src/app/modules/dashboard/components/carrousel-chico/carrousel-chico.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrousel-chico',
+  templateUrl: './carrousel-chico.component.html',
+  styleUrls: ['./carrousel-chico.component.scss']
+})
+export class CarrouselChicoComponent {
+
+}
